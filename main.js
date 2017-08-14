baacdb2e8c10d8cb9c389c686bcc9789732ac615
Sean McPherson <seanmcp25@gmail.com>
2017-08-14 15:40:51 -0400

    Hack seconds in the day

--- main.js
+++ main.js
@@ -2,20 +2,27 @@ const moment = require('moment');
 const chalk = require('chalk');
 
 // Day, date, and time
-console.log(`Today is ${chalk.blue(moment().format('dddd, MMMM Do YYYY, h:mm:ss a'))}.`);
+console.log(`It is ${chalk.blue(moment().format('dddd, MMMM Do YYYY, h:mm:ss a'))}.`);
+
 // Day of the year
-console.log(`It is the ${chalk.magenta(moment().dayOfYear() + 'th')} day of the year.`);
+console.log(`It is the ${chalk.magenta(moment().format('DDDo'))} day of the year.`);
+
 // Seconds into the day
-let now = moment.duration("12:10:12: PM", "HH:mm:ss: A").asSeconds();
+let hS = moment().format('H') * 3600;
+let mS = moment().format('m') * 60;
+let sS = moment().format('s') * 1;
+
 console.log(
-  `It is ${chalk.cyan(now)} seconds into the day.`
+  `It is ${chalk.cyan(hS + mS + sS)} seconds into the day.`
 );
+
 // Check for DST
 if(moment().isDST()){
   console.log(`It ${chalk.green('is')} during Daylight Savings Time.`);
 } else {
   console.log(`Is ${chalk.red('is not')} during Daylight Savings Time.`);
 }
+
 // Check for leap year
 if(moment().isLeapYear()){
   console.log(`It ${chalk.green('is')} a leap year.`);
